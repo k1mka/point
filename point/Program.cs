eb6597297b604221cc2156e61002c130b5d0447d
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-05-22 15:01:45 +0300

    Add project files.

---
+++ point/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace point
+{
+    public class Point2D
+    {
+        public int x;
+        public int y;
+
+    }
+
+    public class Point3D
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
